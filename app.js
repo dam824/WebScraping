4a8128d8a1ccf1dc13701245a127eb184e6d24f7
dam824 <damiendagory@gmail.com>
2023-06-22 16:10:44 +0200

    recuperation uniquement des liens test 1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const pretty = require('pretty');
+const puppeteer = require('puppeteer');
 
 //dependance pour forma xls
 
 const json2xls = require('json2xls');
 const fs = require('fs');
+const XLSX = require('xlsx');
+
 
 const baseUrl = "https://www.novagroupe.eu/poeles--1.html";
+const totalPages = 5;
 
 async function scrapeData(url){
     try {
@@ -16,17 +20,16 @@ async function scrapeData(url){
 
         //on stock les produits dans un array
 
-        const products = [];
+        const links = [];
 
-        $('.ann-titre h2').each((index, element) => {
-            /* console.log($(element).text()); */
-            const productTitle = $(element).text();
-            products.push({'Product Title' : productTitle});
+        $('.ann-titre h2 a').each((index, element) => {
+            const href = $(element).attr('href');
+            links.push(href);
         });
 
-        console.log(products);
+        console.log(links);
 
-       return products
+       return links
         
     }catch(error){
         console.log('une erreur est survenue :', error);
@@ -34,27 +37,38 @@ async function scrapeData(url){
     }
 }
 
-async function scrapeAllPages(){
-    const allProducts = [];
 
-    for(let i = 1; i <= 5; i++){
-        const git = `${baseUrl}?page=${i}`;
+
+
+async function scrapeAllPages(){
+    const allLinks = [];
+
+    for(let i = 1; i <= totalPages; i++){
+        const url = baseUrl + i + '.html';
         console.log(`Page ${i}: ${url}`);
 
-        const products = await scrapeData(url);
-        allProducts.push(...products);
+        const links = await scrapeData(url);
+        allLinks.push(...links);
     }
+
+    console.log(allLinks);
 
      //conversion des données json en format excel
 
-     const xls = json2xls(allProducts);
+     /* const xls = json2xls(allLinks); */
 
      //ecriture du fichier excel
 
-     fs.writeFileSync('Poeles-a-bois.xls', xls, 'binary');
-     console.log("donnees exportees vers le fichier excel");
+     /* fs.writeFileSync('Liens-poeles.xls', xls, 'binary');
+     console.log("donnees exportees vers le fichier excel"); */
+     // Conversion des données en format Excel
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(allLinks);
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Liens');
+  const excelFilePath = 'Liens-poeles.xlsx';
+  XLSX.writeFile(workbook, excelFilePath);
+  console.log(`Données exportées vers le fichier Excel : ${excelFilePath}`);
 }
 
 scrapeAllPages();
 
-scrapeData();
